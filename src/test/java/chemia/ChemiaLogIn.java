1d709c360bb74a643f8360e325e2069eb02db70d
Sirigiriobul <121455638+Sirigiriobul@users.noreply.github.com>
2025-11-28 23:39:54 +0530

    Update ChemiaLogIn.java

--- src/test/java/chemia/ChemiaLogIn.java
+++ src/test/java/chemia/ChemiaLogIn.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class ChemiaLogIn {
 public static void main(String[] args) throws InterruptedException {
 
-
+//Practice Commit
 WebDriver driver=new ChromeDriver();
      driver.get("http://10.106.100.234:9098/Chemia/company");
 driver.manage().window().maximize();
